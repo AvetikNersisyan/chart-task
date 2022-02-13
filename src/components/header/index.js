9dd1f27e0290f5facf67d23799bab634be2fa468
Abbafaria <nersisyan.avetik@gmail.com>
2022-02-13 21:35:51 +0400

    some styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,13 +1,18 @@
 import LanguageSwitcher from "./languageSwitcher";
+
 import "./index.css";
+import {NavLink} from "react-router-dom";
 import Account from "./account";
 
 const Header = () => {
     return (
+
         <header className={"header"}>
-            <h1> BigWin365 affiliate</h1>
+            <NavLink to={"/"}> BigWin365 affiliate</NavLink>
+     
             <div className={"language-switcher"}>
                 <LanguageSwitcher/>
+
             </div>
             <Account user={"Max"}/>
         </header>
---
+++ src/components/navigation/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const Navigation = () => {
+    return (
+        <nav className={"navigation"}>
+            <div>
+                <img src={"#"}/>
+            </div>
+        </nav>
+    );
+};
+
+export default Navigation;
